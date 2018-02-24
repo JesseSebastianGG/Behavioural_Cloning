48de1908f2b7adac6bc1e9931562a47fb00d0f48
JesseSebastianGG <jessesebastian@hotmail.com>
2018-02-24 13:24:16 +0000

    drive.py colorspace bug fixed

--- drive.py
+++ drive.py
@@ -12,6 +12,8 @@ from PIL import Image
 from flask import Flask
 from io import BytesIO
 
+import cv2
+
 from keras.models import load_model
 import h5py
 from keras import __version__ as keras_version
@@ -60,7 +62,11 @@ def telemetry(sid, data):
         # The current image from the center camera of the car
         imgString = data["image"]
         image = Image.open(BytesIO(base64.b64decode(imgString)))
+        ### Apply same pre-processing as in "model.py" (not cropping, this happens in Lambda layer, but YUV inputs because this open happens at fit stage)
         image_array = np.asarray(image)
+        # RGB -> YUV
+        image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2YUV)
+ 
         steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
 
         throttle = controller.update(float(speed))
